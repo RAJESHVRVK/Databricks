fc5ae9c6dbf09a08981a17f22b66d82a0f5acedf
vrvkrpatnaik@gmail.com <vrvkrpatnaik@gmail.com>
2023-08-29 09:55:31 +0000

    Git-Demmo-Notebook commit

--- Git-Demo-Notebook.py
+++ Git-Demo-Notebook.py
@@ -1,3 +1,8 @@
 # Databricks notebook source
 x = 5
 print(x)
+
+# COMMAND ----------
+
+ df=spark.read.csv('/FileStore/tables/EmployeesData.csv', header=True) 
+ df.show() 
---
+++ demo2.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# MAGIC %run "./Git-Demo-Notebook"
+
+# COMMAND ----------
+
+df2=df.select('FIRST_NAME', 'SALARY')
+
+# COMMAND ----------
+
+df2.show()
+
+# COMMAND ----------
+
+
